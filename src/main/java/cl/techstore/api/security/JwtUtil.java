d0746fb801ba217d78791d8ee22dd669acf2a4e2
sakoch-max <sa.koch@duocuc.cl>
2026-05-05 19:14:57 -0400

    Se cambia el metodo de validacion de token debido a que el anterior era muy ambiguo

--- src/main/java/cl/techstore/api/security/JwtUtil.java
+++ src/main/java/cl/techstore/api/security/JwtUtil.java
@@ -1,5 +1,56 @@
 package cl.techstore.api.security;
 
-public class JwtUtil {
+import java.security.Key;
+import java.util.Date;
 
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    
+    
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    
+    private static final long EXPIRATION_TIME = 3600000; 
+
+   
+    public String generarToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    
+    public String extraerUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+    public boolean validarToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.ExpiredJwtException | 
+                 io.jsonwebtoken.UnsupportedJwtException | 
+                 io.jsonwebtoken.MalformedJwtException | 
+                 io.jsonwebtoken.security.SignatureException | 
+                 IllegalArgumentException e) {
+            
+            
+            System.out.println("Token inválido: " + e.getMessage());
+            return false;
+        }
+    }
 }
